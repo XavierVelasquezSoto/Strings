ab0074041825eea733c008586f1a86a24137ab88
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-10-18 19:13:39 +0200

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,183 @@
+function upper(textA) {
+  if (textA.length > 5) {
+    console.log(textA.toUpperCase());
+  } else {
+    console.log(textA.toLowerCase());
+  }
+}
+
+upper('holakjjnkjm');
+
+/* 
+const number = 'hola';
+
+const wordUpper = word.toUpperCase();
+
+console.log(word);
+console.log(wordUpper);
+
+*/
+
+/*
+console.log(5 + '5');
+
+console.log(true + '5');
+
+console.log(5 + 'h'); // Nan(not a number)
+
+console.log(5 + '5');
+
+console.log(5 * true);          // true(tiene un valor de 1)
+
+console.log(5 * false);         // false(tiene un valor de 0)
+
+console.log(5 / false);
+  */
+
+/* 
+String
+Number
+Boolean 
+*/
+
+/* 
+
+const word = 'hola';
+const value = 7;
+
+console.log(value);
+console.log(string(value) + 1);
+
+
+const word = 'hola';
+const value = 7;
+
+console.log(Number(true));
+
+
+const word = 'hola';
+const value = 0;            //todo los números dan true, pero el 0 da false
+
+console.log(Boolean(value));
+
+ */
+
+/*  
+const value = 7;
+
+    if(value === 7) {
+    console.log('OK');
+    }else{
+    console.log('not ok');
+}
+*/
+
+/*  
+const value = 7;
+
+    if(value % 2 === 0){
+    console.log('ES PAR');
+    }else{
+    console.log('NO ES PAR');
+}
+
+// condiciion ? true : false
+
+value % 2 === ? console.log('ES PAR') : console.log('NO ES PAR')
+
+
+const value = 7;
+
+return value % 2 === 0 ? 'ES PAR' : 'NO ES PAR'
+
+*/
+
+/*
+
+Math.ceil(); // azul es una propiedad y moradas son metodos deben llevar parentesis ()
+
+console.log(Math.floor(4.4));
+
+console.log(Math.PI); // PI es el valor númerico
+
+console.log(Math.random() * 11);
+
+console.log(Math.floor(Math.random() * 11));
+
+console.log(Math.floor(Math.random() * Math.PI));
+
+const worde = 'Hola';
+
+console.log(word.length);
+  */
+// charAt -> characterArt
+/*  
+const text = 'Hola';
+
+console.log(world.charAt(3));
+*/
+//
+
+/*  
+const word2 = 'palabra';
+
+console.log(word.charAt(word.length - 1));
+*/
+
+//
+/* 
+const word = xavier;
+
+console.log(word.includes('p'));
+
+const word3 = 'hola y adios';
+
+console.log(word.indexOf('adios'));
+ */
+/* 
+ const word = 'Hola y Adios';
+ console.log(word.toUpperCase());
+
+ const word = 'Hola y Adios';
+ console.log(word.toLowerCase());
+
+const word = 'Hola y Adios';
+console.log(word.startsWith('Ho'))          // cuando inicia la palbra dara true
+ 
+const word = 'Hola y Adios';
+console.log(word.endsWith('adios'))    // cuando termina el texto dara false
+ 
+const word = 'Hola y Adios';
+console.log(word.endsWith('adios'))
+
+const word = 'Hola y Adios';
+console.log(word.substring(0,4))   
+
+//
+el segundo (4) siempre llega a uno menos por eso dice "Hola". si no pones el segundo valor empezara del número hasta el final//
+
+const word = 'Hola y Adios';
+console.log(word.endsWith('adios'))
+*/
+
+//  console.log(`${name} ha pedido una cada de ${material} de tamaño ${size}. ${note}`);
+
+// Los strings son datos inmutables
+
+/*  
+const word = 'hola'; // lo que esta guardado en word(seria un string) no puede cambiar, por eso no es mayuscula
+
+word.toUpperCase();
+
+console.log(word);
+
+//
+
+const number = 'hola';
+
+const wordUpper = word.toUpperCase();
+
+console.log(word);
+console.log(wordUpper);
+
+*/
